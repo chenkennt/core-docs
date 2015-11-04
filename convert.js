97fa93f6e01891871f1a3f5a7e0e781a90493c92
Ken Chen <kenchen@microsoft.com>
2015-11-04 23:18:15 +0800

    add a script to convert from html to markdown

---
+++ convert.js
@@ -0,0 +1,64 @@
+var fs = require("fs");
+var path = require("path");
+var cheerio = require("cheerio");
+var toMd = require("to-markdown");
+
+function removeWithChildren(element, selector) {
+  element.find(selector).remove();
+}
+
+function remove(element, selector) {
+  var elements = element.find(selector);
+  // children should always appear after parent in jquery result?
+  for (var i = elements.length - 1; i >= 0; i--) {
+    var toRemove = cheerio(elements[i]);
+    toRemove.replaceWith(toRemove.html());
+  }
+}
+
+function replace(element, selector, replacer) {
+  var elements = element.find(selector);
+  // children should always appear after parent in jquery result?
+  for (var i = elements.length - 1; i >= 0; i--) {
+    var toRemove = cheerio(elements[i]);
+    toRemove.replaceWith(replacer(toRemove));
+  }
+}
+
+function toMarkdown(body) {
+  // Convert RTD html to a plain html
+  // 1. Remove <noscript> and its children
+  removeWithChildren(body, "noscript");
+  // 2. Remove permalink for heading
+  removeWithChildren(body, ".headerlink");
+  // 3. Remove italic on all links
+  remove(body, "a em");
+  // 4. Change <cite> to <em>
+  replace(body, "cite", function(e) {
+    return "<em>" + e.html() + "</em>";
+  });
+  // 5. Fix code block with lines
+  replace(body, ".highlighttable", function(e) {
+    return e.find(".highlight");
+  });
+  // 6. Fix code block
+  replace(body, ".highlight > pre", function(e) {
+    return "<pre><code>" + e.html() + "</code></pre>";
+  });
+  // 7. Remove <div> and <span>
+  remove(body, "div, span");
+
+  return toMd(body.html(), { gfm: true });
+}
+
+function convert(src, dst) {
+  fs.readdirSync(src).forEach(function(p) {
+    console.log("converting " + p);
+    var html = fs.readFileSync(path.join(src, p), { encoding: "utf8" });
+    var body = cheerio.load(html)(".document");
+    var md = toMarkdown(body);
+    fs.writeFileSync(path.join(dst, p.slice(0, p.length - path.extname(p).length) + ".md"), md);
+  })
+}
+
+convert(process.argv[2], process.argv[3]);
